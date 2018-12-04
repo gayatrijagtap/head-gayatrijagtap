0e616b2438e7325418f3e95971c26f473dff4441
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-12-04 16:36:25 +0530

    Introduced extractInputs
    this function returns an object of headDetails for all input conditions

---
+++ src/lib.js
@@ -0,0 +1,23 @@
+const extractInputs = function(userArgs) {
+  let headDetails = new Object;
+  let index = 2;
+  let option = userArgs[2].split('')[1];
+  headDetails.noOfLines = 10;
+  if(option == 'n' || option == 'c') {
+    headDetails.option = option;
+    headDetails.noOfLines = userArgs[3];
+    index = 4;
+    if(userArgs[2].split('')[2]) {
+      headDetails.noOfLines = userArgs[2].split('')[2];
+      index = 3;
+    }
+  }
+  if(parseInt(option)) {
+    headDetails.noOfLines = parseInt(option);
+    index = 3;
+  }
+  headDetails.files = userArgs.slice(index,userArgs.length);
+  return headDetails;
+}
+
+exports.extractInputs = extractInputs;
---
+++ test/libTest.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const {extractInputs} = require('../src/lib.js');
+
+describe( 'extractInputs' , function() {
+  it( 'should return headDetails including option when option is given' , function() {
+
+    let userArgs = [,,'-n5','file1'];
+    let expectedOutput = {option:'n',noOfLines:5,files:['file1']};
+    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+
+    userArgs = [,,'-n','5','file1'];
+    expectedOutput = {option:'n',noOfLines:5,files:['file1']};
+
+    userArgs = [,,'-c5','file1','file2'];
+    expectedOutput = {option:'c',noOfLines:5,files:['file1','file2']};
+    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+  });
+
+  it( 'should return headDetails excluding option when option is not given' , function() {
+
+    let userArgs = [,,'-5','file1'];
+    let expectedOutput = {noOfLines:5,files:['file1']};
+    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+
+    userArgs = [,,'-5','file1','file2'];
+    expectedOutput = {noOfLines:5,files:['file1','file2']};
+    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+  });
+
+  it( 'should return headDetails including all the files when multiple files as an input' , function() {
+    let userArgs = [,,'file1','file2'];
+    let expectedOutput = {noOfLines:10,files:['file1','file2']};
+    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+  });
+})
