19ef169374ee98895a2a599f978ee8806de4c1d4
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-12-07 12:55:45 +0530

    fixed indentation issues

--- src/lib.js
+++ src/lib.js
@@ -1,146 +1,154 @@
 //---------------------extractInputs-------------------
-const extractInputs = function( userArgs ) {
-  let headDetails = new Object;
-  headDetails.option = extractOption( userArgs[2] );
+const extractInputs = function(userArgs) {
+  let headDetails = new Object();
+  headDetails.option = extractOption(userArgs[2]);
 
-  let { lines,index } = extractNoOfLines( userArgs.slice(2,4) );
+  let { lines, index } = extractNoOfLines(userArgs.slice(2, 4));
 
   headDetails.noOfLines = lines;
-  headDetails.files = userArgs.slice( index,userArgs.length );
+  headDetails.files = userArgs.slice(index, userArgs.length);
 
   return headDetails;
-}
+};
 
 exports.extractInputs = extractInputs;
 
 //--------------------------extractOption--------------    -
-const extractOption = function( userArgs ) {
-  if(userArgs.match(/^-[a-b d-m o-z A-B D-M O-Z]/) ) {
+const extractOption = function(userArgs) {
+  if (userArgs.match(/^-[a-b d-m o-z A-B D-M O-Z]/)) {
     return userArgs[1];
   }
-  return userArgs.match(/^-c/) ? 'c' : 'n';
-}
+  return userArgs.match(/^-c/) ? "c" : "n";
+};
 
 exports.extractOption = extractOption;
 
 //-------------------------extractNoOfLines-------------------
-const extractNoOfLines = function( userArgs ) {
-
-  if( extractNumber( userArgs ) ) {
-    return extractNumber( userArgs );
+const extractNoOfLines = function(userArgs) {
+  if (extractNumber(userArgs)) {
+    return extractNumber(userArgs);
   }
 
-  let array = userArgs[0].split('');
-  let lines = parseInt( array[1] ) || parseInt( array[1] ) == 0 ? { lines:array[1],index:3 } : { lines:'10',index:2 };
+  let array = userArgs[0].split("");
+  let lines =
+    parseInt(array[1]) || parseInt(array[1]) == 0
+      ? { lines: array[1], index: 3 }
+      : { lines: "10", index: 2 };
 
-  if( parseInt( array[2] ) || parseInt( array[2] ) == 0 ) {
-    return { lines:array[2],index:3 };
+  if (parseInt(array[2]) || parseInt(array[2]) == 0) {
+    return { lines: array[2], index: 3 };
   }
 
-  if( parseInt( userArgs[1] ) || parseInt( userArgs[1] ) == 0 ) {
-    return { lines:userArgs[1],index:4 };
+  if (parseInt(userArgs[1]) || parseInt(userArgs[1]) == 0) {
+    return { lines: userArgs[1], index: 4 };
   }
 
   return lines;
-}
+};
 
 exports.extractNoOfLines = extractNoOfLines;
 
-const extractNumber = function( userArgs ) {
-  let array = userArgs[0].split('');
+const extractNumber = function(userArgs) {
+  let array = userArgs[0].split("");
 
-  if( parseInt( array.slice(1,4).join('') ) ) {
-    return { lines:array.slice(1,4).join(''),index:3 };
+  if (parseInt(array.slice(1, 4).join(""))) {
+    return { lines: array.slice(1, 4).join(""), index: 3 };
   }
 
-  if( parseInt( array.slice(2,5).join('') ) ) {
-    return { lines:array.slice(2,5).join(''),index:3 };
+  if (parseInt(array.slice(2, 5).join(""))) {
+    return { lines: array.slice(2, 5).join(""), index: 3 };
   }
-}
+};
 
 exports.extractNumber = extractNumber;
 
- //-------------------------createHeadLines--------------
-const createHeadLines = function( filename ) {
-  return '==> ' + filename + ' <==';
-}
+//-------------------------createHeadLines--------------
+const createHeadLines = function(filename) {
+  return "==> " + filename + " <==";
+};
 
 exports.createHeadLines = createHeadLines;
 
 //-----------------------------extractLines------------
-const extractLines = function( text,noOfLines ) {
+const extractLines = function(text, noOfLines) {
+  let lines = text.split("\n");
 
-  let lines = text.split('\n');
-
-  if( lines.length <= noOfLines ) {
+  if (lines.length <= noOfLines) {
     noOfLines = lines.length;
   }
 
-  return lines.slice( 0,noOfLines ).join('\n');
-}
+  return lines.slice(0, noOfLines).join("\n");
+};
 
 exports.extractLines = extractLines;
 
 //----------------------extractCharacters---------------
-const extractCharacters = function( text,noOfChars ) {
-  let lines = text.split('');
-  return lines.slice( 0,noOfChars ).join('');
-}
+const extractCharacters = function(text, noOfChars) {
+  let lines = text.split("");
+  return lines.slice(0, noOfChars).join("");
+};
 
 exports.extractCharacters = extractCharacters;
 
 //----------------------handleErrors---------------------
 
-const handleErrors = function( option,noOfLines ) {
-  let optionError = 'head: illegal option -- ' + option[0] + '\nusage:head [-n lines | -c bytes] [file ...]'
-  if( option != 'c' && option != 'n' ) {
+const handleErrors = function(option, noOfLines) {
+  let optionError =
+    "head: illegal option -- " +
+    option[0] +
+    "\nusage:head [-n lines | -c bytes] [file ...]";
+  if (option != "c" && option != "n") {
     return optionError;
   }
-  let lineError = 'head: illegal line count -- ' + noOfLines;
-  let byteError = 'head: illegal byte count -- ' + noOfLines;
+  let lineError = "head: illegal line count -- " + noOfLines;
+  let byteError = "head: illegal byte count -- " + noOfLines;
 
-  if( ( noOfLines <= 0 || noOfLines.match(/[a-z A-Z]/) ) && option == 'c' ) {
+  if ((noOfLines <= 0 || noOfLines.match(/[a-z A-Z]/)) && option == "c") {
     return byteError;
   }
 
-  if( ( noOfLines <= 0 || noOfLines.match(/[a-z A-Z]/) ) && option == 'n' ) {
+  if ((noOfLines <= 0 || noOfLines.match(/[a-z A-Z]/)) && option == "n") {
     return lineError;
   }
-}
+};
 
 exports.handleErrors = handleErrors;
 
 //--------------------------getHead---------------------
-const getHead = function( userArgs,fs ) {
-  let headDetails = extractInputs( userArgs );
-  let { files,option,noOfLines } = headDetails;
-  if(handleErrors( option,noOfLines ) ) {
-    return handleErrors(option,noOfLines);
+const getHead = function(userArgs, fs) {
+  let headDetails = extractInputs(userArgs);
+  let { files, option, noOfLines } = headDetails;
+  if (handleErrors(option, noOfLines)) {
+    return handleErrors(option, noOfLines);
   }
-  let type = { 'n':extractLines , 'c':extractCharacters };
-  let head = '';
-  let delimeter = '';
+  let type = { n: extractLines, c: extractCharacters };
+  let head = "";
+  let delimeter = "";
 
-  for( let file of files ) {
-
-    if( !fs.existsSync( file ) ) {
-      let error = 'head: ' + file + ': No such file or directory';
+  for (let file of files) {
+    if (!fs.existsSync(file)) {
+      let error = "head: " + file + ": No such file or directory";
       head = head + delimeter + error;
-      delimeter = '\n'
+      delimeter = "\n";
       continue;
     }
 
-    let data = fs.readFileSync( file,'utf8' );
+    let data = fs.readFileSync(file, "utf8");
 
-    if( files.length == 1 ) { 
-      return type[option]( data,noOfLines ); 
+    if (files.length == 1) {
+      return type[option](data, noOfLines);
     }
 
-    head = head + delimeter + createHeadLines( file ) + '\n' + type[option]( data,noOfLines );
-    delimeter = '\n\n';
+    head =
+      head +
+      delimeter +
+      createHeadLines(file) +
+      "\n" +
+      type[option](data, noOfLines);
+    delimeter = "\n\n";
   }
 
   return head;
-}
+};
 
 exports.getHead = getHead;
--- test/libTest.js
+++ test/libTest.js
@@ -1,140 +1,162 @@
-const assert = require('assert');
-const {handleErrors,extractNumber,getHead,extractOption,extractNoOfLines,extractCharacters,extractLines,extractInputs,createHeadLines} = require('../src/lib.js');
+const assert = require("assert");
+const {
+  handleErrors,
+  extractNumber,
+  getHead,
+  extractOption,
+  extractNoOfLines,
+  extractCharacters,
+  extractLines,
+  extractInputs,
+  createHeadLines
+} = require("../src/lib.js");
 
 //---------------------extractInputs tests-------------------
-describe( 'extractInputs' , function() {
-  it( 'should return headDetails including option when option is given' , function() {
+describe("extractInputs", function() {
+  it("should return headDetails including option when option is given", function() {
+    let userArgs = [, , "-n5", "file1"];
+    let expectedOutput = { option: "n", noOfLines: 5, files: ["file1"] };
+    assert.deepEqual(extractInputs(userArgs), expectedOutput);
 
-    let userArgs = [,,'-n5','file1'];
-    let expectedOutput = {option:'n',noOfLines:5,files:['file1']};
-    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+    userArgs = [, , "-n", "5", "file1"];
+    expectedOutput = { option: "n", noOfLines: 5, files: ["file1"] };
+    assert.deepEqual(extractInputs(userArgs), expectedOutput);
 
-    userArgs = [,,'-n','5','file1'];
-    expectedOutput = {option:'n',noOfLines:5,files:['file1']};
-    assert.deepEqual(extractInputs(userArgs),expectedOutput);
-
-    userArgs = [,,'-c5','file1','file2'];
-    expectedOutput = {option:'c',noOfLines:5,files:['file1','file2']};
-    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+    userArgs = [, , "-c5", "file1", "file2"];
+    expectedOutput = { option: "c", noOfLines: 5, files: ["file1", "file2"] };
+    assert.deepEqual(extractInputs(userArgs), expectedOutput);
   });
 
-  it( 'should return headDetails excluding option when option is not given' , function() {
+  it("should return headDetails excluding option when option is not given", function() {
+    let userArgs = [, , "-5", "file1"];
+    let expectedOutput = { option: "n", noOfLines: 5, files: ["file1"] };
+    assert.deepEqual(extractInputs(userArgs), expectedOutput);
 
-    let userArgs = [,,'-5','file1'];
-    let expectedOutput = {option:'n',noOfLines:5,files:['file1']};
-    assert.deepEqual(extractInputs(userArgs),expectedOutput);
-
-    userArgs = [,,'-5','file1','file2'];
-    expectedOutput = {option:'n',noOfLines:5,files:['file1','file2']};
-    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+    userArgs = [, , "-5", "file1", "file2"];
+    expectedOutput = { option: "n", noOfLines: 5, files: ["file1", "file2"] };
+    assert.deepEqual(extractInputs(userArgs), expectedOutput);
   });
 
-  it( 'should return headDetails including all the files when multiple files as an input' , function() {
-    let userArgs = [,,'file1','file2'];
-    let expectedOutput = {option:'n',noOfLines:10,files:['file1','file2']};
-    assert.deepEqual(extractInputs(userArgs),expectedOutput);
+  it("should return headDetails including all the files when multiple files as an input", function() {
+    let userArgs = [, , "file1", "file2"];
+    let expectedOutput = {
+      option: "n",
+      noOfLines: 10,
+      files: ["file1", "file2"]
+    };
+    assert.deepEqual(extractInputs(userArgs), expectedOutput);
   });
-})
+});
 
 //-------------------------createHeadLines tests--------------
-describe( 'createHeadLines' , function() {
-  it( 'should return the heading for the given function' , function() {
-    assert.deepEqual(createHeadLines('mars'),'==> mars <==');
-    assert.deepEqual(createHeadLines('sample'),'==> sample <==');
+describe("createHeadLines", function() {
+  it("should return the heading for the given function", function() {
+    assert.deepEqual(createHeadLines("mars"), "==> mars <==");
+    assert.deepEqual(createHeadLines("sample"), "==> sample <==");
   });
-})
+});
 
 //-----------------------------extractLines tests------------
-describe( 'extractLines' , function() {
-  it( 'should return the head with given number of lines' , function() {
+describe("extractLines", function() {
+  it("should return the head with given number of lines", function() {
+    let data = "fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk\n";
+    let expectedOutput = "fhash\nhsakh\nfkdsh";
+    assert.deepEqual(extractLines(data, 3), expectedOutput);
 
-    let data = 'fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk\n'
-    let expectedOutput = 'fhash\nhsakh\nfkdsh'
-    assert.deepEqual(extractLines(data,3),expectedOutput);
-
-    data = 'fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk\n'
-    expectedOutput = 'fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk\n'
-    assert.deepEqual(extractLines(data,7),expectedOutput);
+    data = "fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk\n";
+    expectedOutput = "fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk\n";
+    assert.deepEqual(extractLines(data, 7), expectedOutput);
   });
-})
+});
 
 //----------------------extractCharacters tests---------------
-describe( 'extractCharacters' , function() {
-  it( 'should return the head with the given number of characters' , function() {
-    let data = 'fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk'
-    let expectedOutput = 'fha';
-    assert.deepEqual(extractCharacters(data,3),expectedOutput);
-    expectedOutput = 'fhash\nh';
-    assert.deepEqual(extractCharacters(data,7),expectedOutput);
+describe("extractCharacters", function() {
+  it("should return the head with the given number of characters", function() {
+    let data = "fhash\nhsakh\nfkdsh\nhsaklf\nkjfdhs\ndkfsfk";
+    let expectedOutput = "fha";
+    assert.deepEqual(extractCharacters(data, 3), expectedOutput);
+    expectedOutput = "fhash\nh";
+    assert.deepEqual(extractCharacters(data, 7), expectedOutput);
   });
-})
+});
 
 //--------------------------extractOption tests---------------
-describe( 'extractOption' , function() {
-  it( 'should extract the -c or -n option for given input' , function() {
-    assert.deepEqual(extractOption('-c5'),'c');
-    assert.deepEqual(extractOption('-c'),'c');
-    assert.deepEqual(extractOption('-5'),'n');
+describe("extractOption", function() {
+  it("should extract the -c or -n option for given input", function() {
+    assert.deepEqual(extractOption("-c5"), "c");
+    assert.deepEqual(extractOption("-c"), "c");
+    assert.deepEqual(extractOption("-5"), "n");
   });
-})
+});
 
 //-------------------------extractNoOfLines tests-------------------
-describe( 'extractNoOfLines' , function() {
-  it( 'should extract the no of lines from the given input' , function() {
-    assert.deepEqual(extractNoOfLines(['-c5','']),{lines:5,index:3});
-    assert.deepEqual(extractNoOfLines(['-5','']),{lines:5,index:3});
-    assert.deepEqual(extractNoOfLines(['-c','2']),{lines:2,index:4});
-    assert.deepEqual(extractNoOfLines(['file1','file2']),{lines:10,index:2});
+describe("extractNoOfLines", function() {
+  it("should extract the no of lines from the given input", function() {
+    assert.deepEqual(extractNoOfLines(["-c5", ""]), { lines: 5, index: 3 });
+    assert.deepEqual(extractNoOfLines(["-5", ""]), { lines: 5, index: 3 });
+    assert.deepEqual(extractNoOfLines(["-c", "2"]), { lines: 2, index: 4 });
+    assert.deepEqual(extractNoOfLines(["file1", "file2"]), {
+      lines: 10,
+      index: 2
+    });
   });
-})
+});
 
 //--------------------------getHead tests---------------------
 
-describe( 'getHead' , function() {
-
+describe("getHead", function() {
   let readFileSync = file => file;
   let existsSync = file => true;
-  let fs = {readFileSync,existsSync};
+  let fs = { readFileSync, existsSync };
 
-  it( 'should return head of the file with given specifications' , function() {
-    let data = 'fsdjfhsdh\ndfjkshjk\ndsfjdfdkjfs';
-    let userArgs = [,,'-n2',data]
-    assert.deepEqual(getHead(userArgs,fs),'fsdjfhsdh\ndfjkshjk');
+  it("should return head of the file with given specifications", function() {
+    let data = "fsdjfhsdh\ndfjkshjk\ndsfjdfdkjfs";
+    let userArgs = [, , "-n2", data];
+    assert.deepEqual(getHead(userArgs, fs), "fsdjfhsdh\ndfjkshjk");
 
-    data = 'grldfjd';
-  userArgs = [,,'-c','4',data];
-    assert.deepEqual(getHead(userArgs,fs),'grld');
+    data = "grldfjd";
+    userArgs = [, , "-c", "4", data];
+    assert.deepEqual(getHead(userArgs, fs), "grld");
   });
-})
+});
 
 //----------------------validateNoOfLines tests--------------
-describe( 'extractNumber' , function() {
-  it( 'should extract Number from the given input' , function() {
-    assert.deepEqual(extractNumber(['-n','1']));
-    assert.deepEqual(extractNumber(['-n12','temp.js']),{lines:'12',index:3});
-    assert.deepEqual(extractNumber(['-100','temp.js']),{lines:'100',index:3});
+describe("extractNumber", function() {
+  it("should extract Number from the given input", function() {
+    assert.deepEqual(extractNumber(["-n", "1"]));
+    assert.deepEqual(extractNumber(["-n12", "temp.js"]), {
+      lines: "12",
+      index: 3
+    });
+    assert.deepEqual(extractNumber(["-100", "temp.js"]), {
+      lines: "100",
+      index: 3
+    });
   });
-})
+});
 
 //-------------------------handleErrors tests----------------
-describe( 'handleErrors' , function() {
-  it( 'should return error message for illegal option' , function() {
-  let optionError = 'head: illegal option -- ' + 's' + '\nusage:head [-n lines | -c bytes] [file ...]'
-    assert.deepEqual( handleErrors('s','12'),optionError);
-    assert.deepEqual( handleErrors('sd','1'),optionError);
+describe("handleErrors", function() {
+  it("should return error message for illegal option", function() {
+    let optionError =
+      "head: illegal option -- " +
+      "s" +
+      "\nusage:head [-n lines | -c bytes] [file ...]";
+    assert.deepEqual(handleErrors("s", "12"), optionError);
+    assert.deepEqual(handleErrors("sd", "1"), optionError);
   });
 
-  it( 'should return error message for illegal line count' , function() {
-  let lineError = 'head: illegal line count -- ' + '0';
-    assert.deepEqual( handleErrors('n',0),lineError);
-  lineError = 'head: illegal line count -- ' + '-1';
-    assert.deepEqual( handleErrors('n',-1),lineError);
+  it("should return error message for illegal line count", function() {
+    let lineError = "head: illegal line count -- " + "0";
+    assert.deepEqual(handleErrors("n", 0), lineError);
+    lineError = "head: illegal line count -- " + "-1";
+    assert.deepEqual(handleErrors("n", -1), lineError);
   });
 
-  it( 'should return error message for illegal byte count' , function() {
-  let byteError = 'head: illegal byte count -- ' + '0';
-    assert.deepEqual( handleErrors('c',0),byteError);
-  byteError = 'head: illegal byte count -- ' + '-1';
-    assert.deepEqual( handleErrors('c',-1),byteError);
+  it("should return error message for illegal byte count", function() {
+    let byteError = "head: illegal byte count -- " + "0";
+    assert.deepEqual(handleErrors("c", 0), byteError);
+    byteError = "head: illegal byte count -- " + "-1";
+    assert.deepEqual(handleErrors("c", -1), byteError);
   });
-})
+});
