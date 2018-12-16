947a0d79fe55a7e8fd3f532a663e7fbc5c53c4ef
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-12-16 11:29:07 +0530

    Refactored:main function.

--- head.js
+++ head.js
@@ -2,7 +2,8 @@ const {extractInputs,getHead} = require('./src/lib.js');
 const fs = require('fs');
 
 const main = function() {
-  console.log(getHead(process.argv,fs));
+  let userArgs = process.argv;
+  console.log(getHead(userArgs.slice(2),fs));
 }
 
 main();
--- tail.js
+++ tail.js
@@ -2,26 +2,8 @@ const {getTail} = require('./src/lib.js');
 const fs = require('fs');
 
 const main = function() {
-  console.log(getTail(process.argv,fs));
+  let userArgs = process.argv;
+  console.log(getTail(userArgs.slice(2),fs));
 }
 
 main();
-
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
-
-
-
