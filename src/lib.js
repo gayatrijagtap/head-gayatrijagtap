aa0495cba451feb269f752abcc5f420034b05f1f
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-12-17 15:03:54 +0530

    Exported all the functions at the end of the file

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
 //---------------------extractInputs-------------------
 
-const extractInputs = function(userArgs) {
+const extractInputs = function (userArgs) {
   let headDetails = new Object();
   headDetails.option = extractOption(userArgs[0]);
   let { lines, index } = noOfLinesWithFileIndex(userArgs.slice(0, 2));
@@ -13,7 +13,7 @@ exports.extractInputs = extractInputs;
 
 //--------------------------extractOption---------------
 
-const extractOption = function(optionCandidate) {
+const extractOption = function (optionCandidate) {
   return userOption(optionCandidate) || 'n';
 };
 
@@ -32,23 +32,23 @@ exports.userOption = userOption;
 //-------------------------noOfLinesWithFileIndex-------------------
 
 const noOfLinesWithFileIndex = function (userArgs) {
-  if(userArgs[0].match(/^-[0-9]/)) {
-    return {lines:userArgs[0].slice(1),index:1};
+  if (userArgs[0].match(/^-[0-9]/)) {
+    return { lines: userArgs[0].slice(1), index: 1 };
   }
-  if(userArgs[0].match(/^-.[0-9]/)) {
-    return {lines:userArgs[0].slice(2),index:1};
+  if (userArgs[0].match(/^-.[0-9]/)) {
+    return { lines: userArgs[0].slice(2), index: 1 };
   }
-  if(userArgs[0].match(/^-/) && userArgs[1].match(/[0-9]/)) {
-    return {lines:userArgs[1],index:2}
+  if (userArgs[0].match(/^-/) && userArgs[1].match(/[0-9]/)) {
+    return { lines: userArgs[1], index: 2 }
   }
-  return {lines:'10',index:0};
+  return { lines: '10', index: 0 };
 };
 
 exports.noOfLinesWithFileIndex = noOfLinesWithFileIndex;
 
 //-------------------------createHeadLines--------------
 
-const createHeadLines = function(filename) {
+const createHeadLines = function (filename) {
   return "==> " + filename + " <==";
 };
 
@@ -57,9 +57,9 @@ exports.createHeadLines = createHeadLines;
 
 //-----------------------------extractHeadLines------------
 
-const extractHeadLines = function(text, noOfLines) {
+const extractHeadLines = function (text, noOfLines) {
   let lines = text.split("\n");
-  noOfLines = smallerNumber(noOfLines,lines.length);
+  noOfLines = smallerNumber(noOfLines, lines.length);
   return lines.slice(0, noOfLines).join("\n");
 };
 
@@ -67,7 +67,7 @@ exports.extractHeadLines = extractHeadLines;
 
 //-----------------------------smallerNumber------------------
 
-const smallerNumber = function(firstNumber,secondNumber) {
+const smallerNumber = function (firstNumber, secondNumber) {
   return firstNumber >= secondNumber ? secondNumber : firstNumber;
 }
 
@@ -75,7 +75,7 @@ exports.smallerNumber = smallerNumber;
 
 //----------------------extractHeadCharacters---------------
 
-const extractHeadCharacters = function(text, noOfChars) {
+const extractHeadCharacters = function (text, noOfChars) {
   let characters = text.split("");
   return characters.slice(0, noOfChars).join("");
 };
@@ -84,17 +84,17 @@ exports.extractHeadCharacters = extractHeadCharacters;
 
 //----------------------handleHeadErrors---------------------
 
-const handleHeadErrors = function(option, noOfLines) {
-  return invalidHeadOptionError(option) || invalidCountError(noOfLines,option);
+const handleHeadErrors = function (option, noOfLines) {
+  return invalidHeadOptionError(option) || invalidCountError(noOfLines, option);
 };
 
 exports.handleHeadErrors = handleHeadErrors;
 
 //-------------------------------invalidHeadOptionError---------------------
 
-const invalidHeadOptionError = function(optionCandidate) {
+const invalidHeadOptionError = function (optionCandidate) {
   let optionError = "head: illegal option -- " + optionCandidate[0] + "\nusage:head [-n lines | -c bytes] [file ...]";
-  if(optionCandidate != 'c' && optionCandidate != 'n') {
+  if (optionCandidate != 'c' && optionCandidate != 'n') {
     return optionError;
   }
 }
@@ -103,7 +103,7 @@ exports.invalidHeadOptionError = invalidHeadOptionError;
 
 //------------------------------invalidCountError-------------------------
 
-const invalidCountError = function(count,option) {
+const invalidCountError = function (count, option) {
   let countError = new Object;
   countError['n'] = "head: illegal line count -- " + count;
   countError['c'] = "head: illegal byte count -- " + count;
@@ -115,10 +115,10 @@ const invalidCountError = function(count,option) {
 exports.invalidCountError = invalidCountError;
 
 //-----------------------------------extractTailLines---------------------------
-  
+
 const extractTailLines = function (text, noOfLines) {
   let lines = text.split('\n')
-  let trailingLines = getTrailingLines(noOfLines,lines.length);
+  let trailingLines = getTrailingLines(noOfLines, lines.length);
   return lines.slice(trailingLines, lines.length).join('\n');
 }
 
@@ -126,25 +126,25 @@ exports.extractTailLines = extractTailLines;
 
 //---------------------------getTrailingLines-------------------------
 
-const getTrailingLines = function(noOfLines,length) {
-  return noOfLines >= length ? 0 : Math.abs( length-noOfLines );
+const getTrailingLines = function (noOfLines, length) {
+  return noOfLines >= length ? 0 : Math.abs(length - noOfLines);
 }
 
 exports.getTrailingLines = getTrailingLines;
 
 //----------------------extractHeadCharacters---------------
-  
+
 const extractTailCharacters = function (text, noOfChars) {
   let characters = text.split('')
-  let trailingChars = getTrailingLines(noOfChars,characters.length);
+  let trailingChars = getTrailingLines(noOfChars, characters.length);
   return characters.slice(trailingChars, characters.length).join('')
 }
 
 exports.extractTailCharacters = extractTailCharacters;
 
 //------------------------------handleTailErrors------------------------
-  
-const handleTailErrors = function(option, noOfLines) {
+
+const handleTailErrors = function (option, noOfLines) {
   return invalidTailOptionError(option) || isNoOfLinesZero(noOfLines) || illegalOffsetError(noOfLines);
 };
 
@@ -152,9 +152,9 @@ exports.handleTailErrors = handleTailErrors;
 
 //-----------------------------------invalidTailOptionError--------------------
 
-const invalidTailOptionError = function(option) {
+const invalidTailOptionError = function (option) {
   let optionError = 'tail: illegal option -- ' + option + 'usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]';
-  if(option != 'c' && option != 'n') {
+  if (option != 'c' && option != 'n') {
     return optionError;
   }
 }
@@ -163,8 +163,8 @@ exports.invalidTailOptionError = invalidTailOptionError;
 
 //--------------------------------isNoOfLinesZero-----------------------------
 
-const isNoOfLinesZero = function(noOfLines) {
-  if(noOfLines == 0) {
+const isNoOfLinesZero = function (noOfLines) {
+  if (noOfLines == 0) {
     return ' ';
   }
 }
@@ -173,7 +173,7 @@ exports.isNoOfLinesZero = isNoOfLinesZero;
 
 //---------------------------------illegalOffsetError-----------------------
 
-const illegalOffsetError = function(noOfLines) {
+const illegalOffsetError = function (noOfLines) {
   let errorMessage = 'tail: illegal offset -- ' + noOfLines;
   if (noOfLines.match(/[a-z A-Z]/)) {
     return errorMessage;
@@ -184,11 +184,11 @@ exports.illegalOffsetError = illegalOffsetError;
 
 //------------------------------missingFileError-------------------
 
-const missingFileError = function(file,existsSync,command) {
+const missingFileError = function (file, existsSync, command) {
   let head = "head: " + file + ": No such file or directory";
   let tail = 'tail: ' + file + ": No such file or directory";
-  let error = {head,tail};
-  if(!existsSync(file)) {
+  let error = { head, tail };
+  if (!existsSync(file)) {
     return error[command];
   }
 };
@@ -197,51 +197,84 @@ exports.missingFileError = missingFileError;
 
 //-----------------------------singleFileOutput-----------------------
 
-const singleFileOutput = function(headDetails,type,fs,command) {
-  let {files,option,noOfLines} = headDetails;
-  if(files.length == 1 && fs.existsSync(files[0])) {
-    return missingFileError(files[0],fs.existsSync,command) || type[option](fs.readFileSync(files[0],'utf8'),noOfLines);
+const singleFileOutput = function (headDetails, type, fs, command) {
+  let { files, option, noOfLines } = headDetails;
+  if (files.length == 1 && fs.existsSync(files[0])) {
+    return missingFileError(files[0], fs.existsSync, command) || type[option](fs.readFileSync(files[0], 'utf8'), noOfLines);
   }
 }
 
 exports.singleFileOutput = singleFileOutput;
 
-const getHead = function(userArgs,fs) {
+//----------------------------getHead-------------------------------
+
+const getHead = function (userArgs, fs) {
   let headDetails = extractInputs(userArgs);
-  let {files,option,noOfLines} = headDetails;
-  return handleHeadErrors(option,noOfLines) || head(headDetails,fs);
+  let { files, option, noOfLines } = headDetails;
+  return handleHeadErrors(option, noOfLines) || head(headDetails, fs);
 };
 
 exports.getHead = getHead;
 
-const head = function(headDetails,fs) {
-  let {files,option,noOfLines} = headDetails;  
-  let type = {n:extractHeadLines , c:extractHeadCharacters};
+//-------------------------------head---------------------------
+
+const head = function (headDetails, fs) {
+  let { files, option, noOfLines } = headDetails;
+  let type = { n: extractHeadLines, c: extractHeadCharacters };
   let linesAtTop = "";
-  for(let file of files) { 
-    linesAtTop = linesAtTop + (missingFileError(file,fs.existsSync,'head') || createHeadLines(file) +'\n'+ type[option](fs.readFileSync(file,'utf8'),noOfLines))+'\n\n';
+  for (let file of files) {
+    linesAtTop = linesAtTop + (missingFileError(file, fs.existsSync, 'head') || createHeadLines(file) + '\n' + type[option](fs.readFileSync(file, 'utf8'), noOfLines)) + '\n\n';
   }
-  return singleFileOutput(headDetails,type,fs,'head') || linesAtTop;
+  return singleFileOutput(headDetails, type, fs, 'head') || linesAtTop;
 }
 
 exports.head = head;
 
-const getTail = function(userArgs,fs) {
+//----------------------------------getTail------------------------
+
+const getTail = function (userArgs, fs) {
   let tailDetails = extractInputs(userArgs);
-  let {files,option,noOfLines} = tailDetails;
-  return handleTailErrors(option,noOfLines) || tail(tailDetails,fs);
+  let { files, option, noOfLines } = tailDetails;
+  return handleTailErrors(option, noOfLines) || tail(tailDetails, fs);
 };
 
 exports.getTail = getTail;
 
-const tail = function(tailDetails,fs) {
-  let {files,option,noOfLines} = tailDetails;  
-  let type = {n:extractTailLines , c:extractTailCharacters};
+//------------------------------------tail------------------------
+
+const tail = function (tailDetails, fs) {
+  let { files, option, noOfLines } = tailDetails;
+  let type = { n: extractTailLines, c: extractTailCharacters };
   let linesAtBottom = "";
-  for(let file of files) { 
-    linesAtBottom = linesAtBottom + (missingFileError(file,fs.existsSync,'tail') || createHeadLines(file) +'\n'+ type[option](fs.readFileSync(file,'utf8'),noOfLines))+'\n\n';
+  for (let file of files) {
+    linesAtBottom = linesAtBottom + (missingFileError(file, fs.existsSync, 'tail') || createHeadLines(file) + '\n' + type[option](fs.readFileSync(file, 'utf8'), noOfLines)) + '\n\n';
   }
-  return singleFileOutput(tailDetails,type,fs,'tail') || linesAtBottom;
+  return singleFileOutput(tailDetails, type, fs, 'tail') || linesAtBottom;
 }
 
 exports.tail = tail;
+
+module.exports = {
+  singleFileOutput,
+  missingFileError,
+  illegalOffsetError,
+  isNoOfLinesZero,
+  invalidTailOptionError,
+  getTrailingLines,
+  smallerNumber,
+  invalidCountError,
+  invalidHeadOptionError,
+  userOption,
+  getTail,
+  handleTailErrors,
+  extractTailCharacters,
+  extractTailLines,
+  handleHeadErrors,
+  getHead,
+  extractOption,
+  noOfLinesWithFileIndex,
+  extractHeadCharacters,
+  extractHeadLines,
+  extractInputs,
+  createHeadLines
+};
