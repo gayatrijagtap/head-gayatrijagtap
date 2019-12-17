07e40c1d930c637d9f01c8fb42db9192bef079b7
gayatrijagtap <gdjagtap1999@gmail.com>
2019-12-17 15:00:57 +0530

    avoid redundant references in constants

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,10 @@
-const HEAD = 'head';
-const TAIL = 'tail';
-const COLON = ":";
-const SPACE = " ";
-const EMPTY_CHAR = '';
-const NEW_LINE = '\n';
-const RIGHT_FORMATTED_ARROW = "==> ";
-const LEFT_FORMATTED_ARROW = " <==";
-
 module.exports = {
-    HEAD : HEAD,
-    TAIL : TAIL,
-    COLON : COLON,
-    SPACE : SPACE,
-    EMPTY_CHAR : EMPTY_CHAR,
-    NEW_LINE : NEW_LINE,
-    RIGHT_FORMATTED_ARROW : RIGHT_FORMATTED_ARROW,
-    LEFT_FORMATTED_ARROW : LEFT_FORMATTED_ARROW
+    HEAD : 'head',
+    TAIL : 'tail',
+    COLON : ":",
+    SPACE : " ",
+    EMPTY_CHAR : '',
+    NEW_LINE : '\n',
+    RIGHT_FORMATTED_ARROW : "==> ",
+    LEFT_FORMATTED_ARROW : " <=="
 }
